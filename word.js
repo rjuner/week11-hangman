4156bc6567611b6d4dd175c81d2a406921e2c08e
Rhea Rivera <rjuner@gmail.com>
2016-07-08 20:07:29 -0500

    getLets and didWeFindTheWord functions are working

--- word.js
+++ word.js
@@ -3,13 +3,33 @@
 //requiring the "letter.js" file
 var letter = require('./letter.js');
 
+var testWord = 'pizza'; 
+
+var lets = [];
+
 var Word = function(wrd){
 	this.word = wrd, 
-	this.lets = [], 
+	this.lets = lets, 
 	this.found = false, 
+
+	//loop over the word property and push letter objects into the lets property
 	this.getLets = function(){
 		for(var i = 0; i < wrd.length; i++){
-			console.log(wrd[i]);
+			//console.log(wrd[i]);
+			lets.push(wrd[i]);
+		}
+	}, 
+
+	//returns true or false whether we found the current word or not
+	this.didWeFindTheWord = function(){
+		var count = 0; 
+		for(var i = 0; i < testWord.length; i++){
+			//console.log(testWord[i]);
+			if(testWord[i] == lets[i]){
+				console.log("SAME LETTER!"); 
+			}else{
+				console.log("NOT THE SAME!");
+			}
 		}
 	}
 }
@@ -17,3 +37,7 @@ var Word = function(wrd){
 var test = new Word('donut'); 
 
 test.getLets();
+test.didWeFindTheWord();
+
+//+++++CONSOLE TESTS
+	// console.log(lets);
